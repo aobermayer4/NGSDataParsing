a3c85847e5502a27eae4d7ee1ff2ff37a258a0f8
Alyssa Obermayer <aobermayer4@gmail.com>
2021-07-02 14:13:34 -0400

    Script now renames files and folders in path

--- RenamerByMap.py
+++ RenamerByMap.py
@@ -13,15 +13,31 @@ else :
 
 ## make pandas df with namemap
 namemap=pd.read_csv(namemap, sep='\t', index_col=0)
+#spaces in header were manually taken out. Will be looking into coding that piece in.
 
 
-for folder in os.listdir(filepath) :
+## will rename from deepst file out to folders in directory
+#  rename is based off of 'SampleID' and first character of 'SampleType' found in 'namemap' + ext's
+#  some 'SampleID's have 2 sample of the same type and are seperated by a comma and space.
+#  will look into aking the white space out.
+
+for folder in os.listdir(filepath) : #loop through given path for sub-directories
+	for file in os.listdir(os.path.join(filepath, folder)) : #loop through folders for files/sub-dir
+		if os.path.isdir(os.path.join(filepath, folder, file)) == True : #if sub-dir exists
+			for file2 in os.listdir(os.path.join(filepath,folder, file)) : #loop through sub-dir for files
+				ext=file2.split('.')[1:] #extraxt file extensions
+				ext='.'.join(ext) #join extensions
+				src=os.path.join(filepath, folder, file, file2) #old path to file
+				dst=os.path.join(filepath, folder, file, #new path to file
+					namemap.at[folder,'SampleID']+'_'+namemap.at[folder,'SampleType'][0]+'.'+str(ext))
+				os.rename(src,dst) #rename function
+		else :
+			ext=file.split('.')[1:]
+			ext='.'.join(ext)
+			src=os.path.join(filepath, folder, file)
+			dst=os.path.join(filepath, folder,
+				namemap.at[folder,'SampleID']+'_'+namemap.at[folder,'SampleType'][0]+'.'+str(ext))
+			os.rename(src,dst)
 	src=os.path.join(filepath, folder)
 	dst=os.path.join(filepath, namemap.at[folder,'SampleID']+'_'+namemap.at[folder,'SampleType'][0])
 	foldernew=os.rename(src, dst)
-	print(foldernew)
-
-
-#### IN PROGRESS ####
-# will be adding script to rename files within folders
-# just troubleshooting (:
