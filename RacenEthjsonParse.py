092e778c23ee4f6b401867f9d5175936b0a50f0f
Alyssa Obermayer <aobermayer4@gmail.com>
2021-07-05 23:51:52 -0400

    Initial Commit of small/specific json parser

---
+++ RacenEthjsonParse.py
@@ -0,0 +1,13 @@
+#! /bin/python3.9.5
+
+import sys
+import json
+
+data = json.load(open(sys.argv[1], 'r'))
+
+for i in data:
+	caseID = i['exposures'][0]['submitter_id']
+	race = i['demographic']['race']
+	ethnicity = i['demographic']['ethnicity']
+	status = i['demographic']['vital_status']
+	print('caseID:',caseID,'\n\trace:',race,'\n\tethnicity:',ethnicity,'\n\tvital_status:',status)
