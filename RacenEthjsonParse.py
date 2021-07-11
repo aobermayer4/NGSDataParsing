9eefedfb4f26ec4815a8fb9256b6e989d9da9dbd
Alyssa Obermayer <aobermayer4@gmail.com>
2021-07-11 03:45:04 -0400

    Now writes to file, but see ClinicalParser.py instead

--- RacenEthjsonParse.py
+++ RacenEthjsonParse.py
@@ -2,8 +2,14 @@
 
 import sys
 import json
+import numpy as np
+
 
 data = json.load(open(sys.argv[1], 'r'))
+clinout = open('ClinicalStats.tsv', 'w')
+
+clinical=np.array(['CaseID','Race','Ethnicity','Vital_Status','Smoker_Status']) #blank np array
+
 
 for i in data:
 	caseID = i['exposures'][0]['submitter_id']
@@ -12,8 +18,25 @@ for i in data:
 	ethnicity = i['demographic']['ethnicity']
 	vitals = i['demographic']['vital_status']
 	smoker = i['exposures'][0]['tobacco_smoking_status']
-	print('caseID:',caseID,
-		'\n\trace:',race,
-		'\n\tethnicity:',ethnicity,
-		'\n\tvital_status:',vitals,
-		'\n\tTobacco_Smoking_Status:',smoker)
+	clinical=np.vstack((clinical,[caseID,race,ethnicity,vitals,smoker]))
+
+np.savetxt(clinout, clinical, fmt='%s', delimiter='\t')
+
+clinout.close()
+
+
+
+
+
+
+
+
+
+
+
+
+#	print('caseID:',caseID,
+#		'\n\trace:',race,
+#		'\n\tethnicity:',ethnicity,
+#		'\n\tvital_status:',vitals,
+#		'\n\tTobacco_Smoking_Status:',smoker)
